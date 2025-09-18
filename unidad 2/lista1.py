c0ffc31a32008b376a7c283dd0c27ce35bf07acf
paulosantyp <paulosantyp1@gmail.com>
2025-09-18 07:54:49 -0600

    Create lista1.py

---
+++ unidad 2/lista1.py
@@ -0,0 +1,15 @@
+#[] Es para listas
+Lista=["Juan", "Ivan", "Adrian", "Jair"]
+print (Lista)
+#seleccionar un elemento de la lista
+print (Lista[2])
+#append es solo para agregar un solo elemento
+Lista.append("alexa")
+print (Lista)
+Lista.insert(1,"Javier")
+print (Lista)
+#pop es para eliminar un elemento de la lista
+Lista.pop()
+print (Lista)
+Lista.pop(1)
+print (Lista)
